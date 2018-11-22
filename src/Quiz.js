5f606bda43558fdbdd3678583e0b26370377111e
Ajeesh G <Ajeeshg@gmail.com>
2018-11-22 05:51:16 -0500

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,7 +9,7 @@ class Quiz extends Component {
         this.state = {quiz_position: 1}
     }
     render(){
-        const isQuizEnd = ((this.state.quiz_position -1) === (quizData.quiz_questions.length));
+        const isQuizEnd = ((this.state.quiz_position -1) === quizData.quiz_questions.length)
         return (
             //<div><div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div></div>
             <div> 
